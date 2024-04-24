3bc31ee3dc838cc565222ac23bd0988cba75804b
Lavr <110830098+StasLavr@users.noreply.github.com>
2024-04-24 11:09:36 +0500

    Update env.nix

--- system/configuration/env.nix
+++ system/configuration/env.nix
@@ -1,6 +1,6 @@
 { inputs, config, pkgs, ... }:
 {
-  environment.sessionVariables.FLAKE = "/home/lavr/nixosconf";
+  environment.sessionVariables.FLAKE = "/home/lavr/.system";
   environment.sessionVariables.WLR_NO_HARDWARE_CURSORS = "1";
   environment.sessionVariables.QT_QPA_PLATFORMTHEME = "qt5ct";
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
